86a522f47f766e3f291f22c00f3024b1de76b6b6
Willian Wiegand <willianwiegand21@gmail.com>
2024-03-10 15:38:34 -0300

    Fixed some loops
    Fixed reading file loops, add option to chose hability

--- src/models/Jogo.java
+++ src/models/Jogo.java
@@ -1,85 +1,42 @@
-package models;
+    package models;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
+    import java.io.BufferedReader;
+    import java.io.FileReader;
+    import java.io.IOException;
+    import java.util.ArrayList;
+    import java.util.Random;
+    import java.util.Scanner;
 
-public class Jogo {
-    private Equipe equipeHerois;
-    private Equipe equipeInimigos;
-    String nomeClasse;
+    public class Jogo {
+        private Equipe equipeHerois;
+        private Equipe equipeInimigos;
+        String nomeClasse;
 
-    public Jogo() {
-        equipeHerois = new Equipe();
-        equipeInimigos = new Equipe();
-    }
+        private int faseAtual = 1;
+        private int cont = 1;
 
-    public void iniciarJogo() {
-        carregarEquipesHerois();
-        carregarEquipesInimigos();
-        exibirInformacoesEquipes();
-
-        System.out.println("Começa a batalha!\n");
-        batalhar();
-    }
-
-    private void carregarEquipesHerois() {
-        Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Digite o nome do herói " + (i + 1) + ": ");
-            String nome = scanner.nextLine();
-            System.out.println("Escolha a classe do herói " + (i + 1) + " (Guerreiro, Arqueiro, Mago ou Monstro): ");
-            String classe = scanner.nextLine();
-            Classe classePersonagem;
-            switch (classe.toLowerCase()) {
-                case "guerreiro":
-                    classePersonagem = new Guerreiro();
-                    break;
-                case "arqueiro":
-                    classePersonagem = new Arqueiro();
-                    break;
-                case "mago":
-                    classePersonagem = new Mago();
-                    break;
-                case "monstro":
-                    classePersonagem = new Monstro();
-                    break;
-                default:
-                    System.out.println("Classe inválida. Será atribuída a classe padrão Guerreiro.");
-                    classePersonagem = new Guerreiro();
-            }
-            Personagem heroi = new Personagem(nome, 1, 0, 100, 100, classePersonagem);
-            equipeHerois.adicionarPersonagem(heroi);
+        public Jogo() {
+            equipeHerois = new Equipe();
+            equipeInimigos = new Equipe();
         }
-        scanner.close();
-    }
 
-    private void carregarEquipesInimigos() {
-        try (BufferedReader br = new BufferedReader(new FileReader("src/utils/jogo.txt"))) {
-            String linha;
-            String faseDescricao = null;
-            while ((linha = br.readLine()) != null) {
-                if (linha.startsWith("fase")) {
-                    System.out.println("\n" + linha);
-                    faseDescricao = br.readLine();
-                    System.out.println(faseDescricao);
-                    continue;
-                }
+        public void iniciarJogo() {
+            carregarEquipesHerois();
+            carregarEquipesInimigos();
+            exibirInformacoesEquipes();
 
-                String[] partes = linha.split(" ");
-                if (partes.length < 3) {
-                    System.out.println("Linha inválida: " + linha);
-                    continue;
-                }
+            System.out.println("Começa a batalha!\n");
+            batalhar();
+        }
 
-                String nome = partes[0];
-                String classe = partes[1];
-                int nivel = Integer.parseInt(partes[2]);
-                int PV = 100;
-                int PM = 100;
+
+        private void carregarEquipesHerois() {
+            Scanner scanner = new Scanner(System.in);
+            for (int i = 0; i < 3; i++) {
+                System.out.println("Digite o nome do herói " + (i + 1) + ": ");
+                String nome = scanner.nextLine();
+                System.out.println("Escolha a classe do herói " + (i + 1) + " (Guerreiro, Arqueiro, Mago ou Monstro): ");
+                String classe = scanner.nextLine();
                 Classe classePersonagem;
                 switch (classe.toLowerCase()) {
                     case "guerreiro":
@@ -95,103 +52,197 @@ public class Jogo {
                         classePersonagem = new Monstro();
                         break;
                     default:
-                        System.out.println("Classe inválida. Será atribuída a classe padrão Guerreiro para o inimigo " + nome);
+                        System.out.println("Classe inválida. Será atribuída a classe padrão Guerreiro.");
                         classePersonagem = new Guerreiro();
                 }
-                Personagem inimigo = new Personagem(nome, nivel, 0, PV, PM, classePersonagem);
-                equipeInimigos.adicionarPersonagem(inimigo);
+                Personagem heroi = new Personagem(nome, 1, 0, 100, 100, classePersonagem);
+                equipeHerois.adicionarPersonagem(heroi);
             }
-        } catch (IOException e) {
-            System.err.println("Erro ao ler o arquivo jogo.txt: " + e.getMessage());
-        }
-    }
 
-
-    private void exibirInformacoesEquipes() {
-        System.out.println("Equipe dos Heróis:");
-        exibirInformacoesEquipe(equipeHerois);
-        System.out.println("\nEquipe dos Inimigos:");
-        exibirInformacoesEquipe(equipeInimigos);
-    }
-
-    private void exibirInformacoesEquipe(Equipe equipe) {
-        ArrayList<Personagem> membros = equipe.getMembros();
-
-        for (Personagem personagem : membros) {
-            System.out.println("ID: " + personagem.getID() + " | Nome: " + personagem.getNome() + " | Classe: " +
-                    personagem.getClasse().getNome() + " | PV: " + personagem.getPV() +
-                    " | PM: " + personagem.getPM() + " | Nível: " + personagem.getNivel() +
-                    " | Tempo de Espera: " + personagem.getTempoEspera());
-        }
-    }
-
-    private void batalhar() {
-        while (!equipeHerois.getMembros().isEmpty() && !equipeInimigos.getMembros().isEmpty()) {
-            Personagem primeiroAtacante = sortearPrimeiroAtacante();
-            System.out.println(primeiroAtacante.getNome() + " é o próximo a atacar!\n");
-            atacar(primeiroAtacante);
-            proximoTurno();
-        }
-        if (equipeHerois.getMembros().isEmpty()) {
-            System.out.println("Os heróis foram derrotados!");
-        } else {
-            System.out.println("Os heróis venceram a batalha!");
-            distribuirPontosExperiencia();
-        }
-    }
-
-    private Personagem sortearPrimeiroAtacante() {
-        Random rand = new Random();
-        int index = rand.nextInt(equipeHerois.getMembros().size() + equipeInimigos.getMembros().size());
-        if (index < equipeHerois.getMembros().size()) {
-            return equipeHerois.getMembros().get(index);
-        } else {
-            return equipeInimigos.getMembros().get(index - equipeHerois.getMembros().size());
-        }
-    }
-
-    private void atacar(Personagem atacante) {
-        Equipe equipeAlvo = equipeInimigos;
-        if (atacante.getClasse() instanceof Guerreiro) {
-            equipeAlvo = equipeHerois;
         }
 
-        ArrayList<Personagem> membrosAlvo = equipeAlvo.getMembros();
-        Random rand = new Random();
-        int indexAlvo = rand.nextInt(membrosAlvo.size());
-        Personagem alvo = membrosAlvo.get(indexAlvo);
+        private void carregarEquipesInimigos() {
+            try (BufferedReader br = new BufferedReader(new FileReader("src/utils/jogo.txt"))) {
+                String linha;
+                while ((linha = br.readLine()) != null) {
+                    if (linha.startsWith("fase")) {
+                        if(faseAtual > 1) {
+                            continue;
+                        }
+                        System.out.println(linha.substring(5) + "\n");
+                        faseAtual++;
+                    }
 
-        int dano = atacante.getClasse().atacar(alvo);
+                    if (linha.isEmpty()) {
+                        // Pula linhas vazias
+                        continue;
+                    }
 
-        alvo.setPV(alvo.getPV() - dano);
 
-        if (alvo.getPV() <= 0) {
-            System.out.println(alvo.getNome() + " foi derrotado!");
-            equipeAlvo.removerPersonagem(alvo);
+                    if(faseAtual == 1) {
+                        String[] partes = linha.split(" ");
+                        if (partes.length < 3) {
+                            System.out.println("Linha inválida: " + linha);
+                            continue;
+                        }
+
+                        String nome = partes[0];
+                        String classe = partes[1];
+                        if (!Character.isDigit(partes[2].charAt(0))) {
+                            // A string não é um número inteiro
+                            continue;
+                        }
+                        int nivel = Integer.parseInt(partes[2]);
+                        int PV = 100;
+                        int PM = 100;
+                        Classe classePersonagem;
+                        switch (classe.toLowerCase()) {
+                            case "guerreiro":
+                                classePersonagem = new Guerreiro();
+                                break;
+                            case "arqueiro":
+                                classePersonagem = new Arqueiro();
+                                break;
+                            case "mago":
+                                classePersonagem = new Mago();
+                                break;
+                            case "monstro":
+                                classePersonagem = new Monstro();
+                                break;
+                            default:
+                                System.out.println("Classe inválida. Será atribuída a classe padrão Guerreiro para o inimigo " + nome);
+                                classePersonagem = new Guerreiro();
+                        }
+                        Personagem inimigo = new Personagem(nome, nivel, 0, PV, PM,classePersonagem);
+                        equipeInimigos.adicionarPersonagem(inimigo);
+                        cont++;
+                    }
+                }
+            } catch (IOException e) {
+                System.err.println("Erro ao ler o arquivo jogo.txt: " + e.getMessage());
+            }
+        }
+
+
+        private void exibirInformacoesEquipes() {
+            System.out.println("Equipe dos Heróis:");
+            exibirInformacoesEquipe(equipeHerois);
+            System.out.println("\nEquipe dos Inimigos:");
+            exibirInformacoesEquipe(equipeInimigos);
+        }
+
+        private void exibirInformacoesEquipe(Equipe equipe) {
+            ArrayList<Personagem> membros = equipe.getMembros();
+
+            for (Personagem personagem : membros) {
+                System.out.println("ID: " + personagem.getID() + " | Nome: " + personagem.getNome() + " | Classe: " +
+                        personagem.getClasse().getNome() + " | PV: " + personagem.getPV() +
+                        " | PM: " + personagem.getPM() + " | Nível: " + personagem.getNivel() +
+                        " | Tempo de Espera: " + personagem.getTempoEspera());
+            }
+        }
+
+        private void batalhar() {
+            while (!equipeHerois.getMembros().isEmpty() && !todosInimigosDerrotados()) {
+                Personagem primeiroAtacante = sortearPrimeiroAtacante();
+                System.out.println(primeiroAtacante.getNome() + " é o próximo a atacar!\n");
+                atacar(primeiroAtacante);
+                proximoTurno();
+            }
+
+            if (todosInimigosDerrotados()) {
+                if (faseConcluida()) {
+                    carregarEquipesInimigos();
+                    exibirInformacoesEquipes();
+                    System.out.println("Começa a próxima batalha!\n");
+                } else {
+                    System.out.println("Os heróis venceram a batalha!");
+                    distribuirPontosExperiencia();
+                }
+            } else {
+                System.out.println("Os heróis foram derrotados!");
+            }
+        }
+
+        private Personagem sortearPrimeiroAtacante() {
+            Random rand = new Random();
+            int index = rand.nextInt(equipeHerois.getMembros().size() + equipeInimigos.getMembros().size());
+            if (index < equipeHerois.getMembros().size()) {
+                return equipeHerois.getMembros().get(index);
+            } else {
+                return equipeInimigos.getMembros().get(index - equipeHerois.getMembros().size());
+            }
+        }
+
+        private void atacar(Personagem atacante) {
+            Scanner scanner = new Scanner(System.in);
+            Equipe equipeAlvo = equipeInimigos;
+            if (atacante.getClasse() instanceof Guerreiro) {
+                equipeAlvo = equipeHerois;
+            }
+
+            ArrayList<Personagem> membrosAlvo = equipeAlvo.getMembros();
+            Random rand = new Random();
+            int indexAlvo = rand.nextInt(membrosAlvo.size());
+            Personagem alvo = membrosAlvo.get(indexAlvo);
+
+            System.out.println("Selecione a habilidade para " + atacante.getNome() + ":");
+            for (int i = 0; i < atacante.getClasse().getHabilidades().size(); i++){
+                System.out.println((i + 1) + " - " + atacante.getClasse().getHabilidades().get(i).getNome());
+            }
+
+
+            int escolhaHabilidade = 0;
+
+            do {
+                try {
+                    escolhaHabilidade = Integer.parseInt(scanner.nextLine());
+                    if (escolhaHabilidade <= 0 || escolhaHabilidade > atacante.getClasse().getHabilidades().size()) {
+                        throw new NumberFormatException();
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Escolha inválida. Digite um número entre 1 e " + atacante.getClasse().getHabilidades().size() + ".");
+                }
+            } while (escolhaHabilidade <= 0 || escolhaHabilidade > atacante.getClasse().getHabilidades().size());
+
+            int dano = atacante.getClasse().atacar(alvo);
+
+            alvo.setPV(alvo.getPV() - dano);
+
+            if (alvo.getPV() <= 0) {
+                System.out.println(alvo.getNome() + " foi derrotado!");
+                equipeAlvo.removerPersonagem(alvo);
+            }
+        }
+
+        private void proximoTurno() {
+            equipeHerois.atualizarTempoEspera();
+            equipeInimigos.atualizarTempoEspera();
+        }
+
+        private void distribuirPontosExperiencia() {
+            int pontosExperiencia = 0;
+            for (Personagem inimigoDerrotado : equipeInimigos.getMembros()) {
+                pontosExperiencia += inimigoDerrotado.getNivel() * 10;
+            }
+
+            ArrayList<Personagem> heroisSobreviventes = equipeHerois.getMembros();
+            int pontosPorHeroi = pontosExperiencia / heroisSobreviventes.size();
+            for (Personagem heroi : heroisSobreviventes) {
+                heroi.setExperiencia(heroi.getExperiencia() + pontosPorHeroi);
+                System.out.println(heroi.getNome() + " ganhou " + pontosPorHeroi + " pontos de experiência!");
+            }
+        }
+        private boolean todosInimigosDerrotados() {
+            return equipeInimigos.getMembros().isEmpty();
+        }
+
+        private boolean faseConcluida(){
+            return faseAtual > 2;
+        }
+
+        public static void main(String[] args) {
+            Jogo jogo = new Jogo();
+            jogo.iniciarJogo();
         }
     }
-
-    private void proximoTurno() {
-        equipeHerois.atualizarTempoEspera();
-        equipeInimigos.atualizarTempoEspera();
-    }
-
-    private void distribuirPontosExperiencia() {
-        int pontosExperiencia = 0;
-        for (Personagem inimigoDerrotado : equipeInimigos.getMembros()) {
-            pontosExperiencia += inimigoDerrotado.getNivel() * 10;
-        }
-
-        ArrayList<Personagem> heroisSobreviventes = equipeHerois.getMembros();
-        int pontosPorHeroi = pontosExperiencia / heroisSobreviventes.size();
-        for (Personagem heroi : heroisSobreviventes) {
-            heroi.setExperiencia(heroi.getExperiencia() + pontosPorHeroi);
-            System.out.println(heroi.getNome() + " ganhou " + pontosPorHeroi + " pontos de experiência!");
-        }
-    }
-
-    public static void main(String[] args) {
-        Jogo jogo = new Jogo();
-        jogo.iniciarJogo();
-    }
-}
